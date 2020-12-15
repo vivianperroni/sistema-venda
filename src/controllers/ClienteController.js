569da2534f49fd7d6a6cbc2763913b287a08f0d9
vivianperroni <vivian.perroni@hotmail.com>
2020-12-15 05:13:02 -0300

    controle  de crud

---
+++ src/controllers/ClienteController.js
@@ -0,0 +1,69 @@
+const db = require('../db/connection')
+
+class ClienteController{
+    
+    index(req,res){
+        db.query('SELECT * FROM cliente', (err,result)=>{
+            if(err){
+              console.log('Houve um erro ao listar cliente: ${err}')
+            }
+              res.render('cliente/listar',{clientes:result.rows})
+          })
+    }
+    create(req,res){
+      res.render('cliente/adicionar')  
+    }
+    store(req,res){
+      const query = {
+        text: 'INSERT INTO cliente(nome,cpf) VALUES($1,$2)',
+        values:[req.body.nome, req.body.cpf]
+      }
+      db.query(query,(err,result)=>{
+        if(err){
+          console.log('Houve um erro: ${err}')
+        }
+        res.redirect('/cliente/listar')
+      })
+    }
+    edit(req,res){
+      console.log(req.params.id)
+      const query = {
+        text:'SELECT * FROM cliente WHERE id = $1',
+        values:[req.params.id]
+      }
+      db.query(query,(err,result)=>{
+        if(err){
+          console.log('Houve um erro ao editar: ${err}')
+        }
+          res.render('cliente/editar',{cliente:result.rows[0]})
+      })
+    }
+    update(req,res){
+      const dados = req.body
+      const query = {
+        text: 'UPDATE cliente SET nome = $1, cpf = $2 WHERE id = $3',
+        values:[dados.nome, dados.cpf, dados.id]
+      }
+      db.query(query,(err,result)=>{
+        if(err){
+          console.log('Houve um erro na atualização: ${err}')
+        }
+          res.redirect('/cliente/listar')
+      })
+    }
+    delete(req,res){
+      const id = req.params.id
+      const query = {
+        text:'DELETE FROM cliente WHERE id = $1',
+        values:[id]
+      }
+      db.query(query,(err,result)=>{
+        if(err){
+          console.log('Houve um erro ao excluir: ${err}')
+        }
+        res.redirect('/cliente/listar')
+      })
+    }
+}
+
+module.exports = new ClienteController()
